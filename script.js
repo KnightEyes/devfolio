77e730c13b12229560340a8e6f3e253b37dc2e4d
KnightEyes <knighteyesdragon@gmail.com>
2026-04-01 21:37:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 fetch("https://knighteyes.github.io/devfolio/data.json")
-  .then(response => alert(response.text()))
-  .catch(error => alert(error));
+  .then(response => console.log(response))
+  .catch(error => console.log(error));

5b3f9158b2a5aea6d8b790a1afb908260a5b3c4c
KnightEyes <knighteyesdragon@gmail.com>
2026-04-01 22:45:30 -0400

    delte

--- script.js
+++
@@ -1,9 +0,0 @@
-
-fetch("https://knighteyes.github.io/devfolio/data.json")
-.then(response => {
-  if(!response.ok)
-    throw new Error("Data not found.");
-  return response.json();
-})
-.then(data => console.log(data))
-.catch(error => console.log(error));

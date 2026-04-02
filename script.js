4a0790f092884c2e3241916c5105e61c685f66e8
KnightEyes <knighteyesdragon@gmail.com>
2026-04-01 22:07:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 async function getData(){
   try{
-    const response = await fetch();
+    const response = await fetch("https://knighteyes.github.io/devfolio/data.json");
   } catch {
     console.error(error);
   }
